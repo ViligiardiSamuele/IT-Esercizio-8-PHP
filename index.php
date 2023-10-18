6eb65125ef24128be13e4a61400054df179f907b
ViligiardiSamuele <viligiardi.samuele@itismeucci.com>
2023-10-18 20:45:12 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+echo '
+    <!DOCTYPE html>
+    <html lang="it">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>IT-Esercizio-8-PHP</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+';
+echo '<h1>Hello world</h1>';
+echo '
+    </body>
+</html>
+';
+?>
